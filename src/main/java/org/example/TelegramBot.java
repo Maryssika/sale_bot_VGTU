c033b0bf27c86f2c442337fcd5326a4b3700eef4
Алексей Зенков <zenkov.m700@mail.ru>
2025-05-30 17:37:19 +0300

    init develop

---
+++ src/main/java/org/example/TelegramBot.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+            message.setText("You said: " + messageText);
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "YourBotName";
+    }
+
+
+
+    @Override
+    public String getBotToken() {
+        return "7472746068:AAG-Uj1CvFFtLiO9r8agC4o5dsGx1XUeZ3I"; // Your bot token
+    }
+}
